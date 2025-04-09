3780ebbf1bff14090b35d117ce7780d888e4d163
Tirtho Saha <220150.cse@student.just.edu.bd>
2025-04-09 09:51:05 +0600

    Add files via upload

---
+++ cf_round_1016A.java
@@ -0,0 +1,25 @@
+
+
+public class cf_round_1016A {
+    public static void main(String[] args) {
+
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        int t=sc.nextInt();
+
+        while (t-->0) {
+            int k=sc.nextInt();
+
+            if(k%2==0){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+            
+
+            
+        }
+
+        
+    }
+}
